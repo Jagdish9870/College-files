8574d6477f828f70a58dcfe6f0871ac54708a1f4
jagdish gupta <varshneyjacky@gmail.com>
2023-10-10 13:40:48 +0530

    -a

---
+++ AreaOfDiffShape.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Shape {
+
+    public int length;
+    public int Width;
+    public int height;
+    public int base;
+
+  
+}
+class Triangle extends Shape{
+        int Area=1/2*height*base;
+    public void area(){
+        System.out.println();
+    }
+}
+
+public class AreaOfDiffShape {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter heigh");
+    }
+    
+}

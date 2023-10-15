504729629270385a7e9ac0eb96dceed25c0e450b
jagdish gupta <varshneyjacky@gmail.com>
2023-10-15 19:56:17 +0530

    -a

---
+++ findAvg.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class findAvg {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+
+        try{
+             System.out.println("enter the numbers to find the avg");
+
+        int n= sc.nextInt();
+        int arr[]= new int[10];
+        System.out.println("enter the numbers");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        
+        int sum=0;
+        for(int j:arr){
+           sum+=j;
+        }
+      
+        int avg=sum/n;
+        System.out.println("average of numbers is : " +avg);
+            
+        }
+        catch(Exception e){
+            System.out.println("you can enter 10 numbers only.......");
+        }
+       
+
+        
+
+
+      sc.close();
+
+        
+    }
+    
+}
